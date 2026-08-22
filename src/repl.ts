455f08b9882d1f7beadba8400c7ebdcb08b62a64
mumairdotdev <m.umair.kn@gmail.com>
2026-08-22 20:15:13 +0500

    C: REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
 
     return input
@@ -5,4 +7,31 @@ export function cleanInput(input: string): string[] {
     .trim()
     .split(" ")
     .filter((word) => word !== "");
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        const cleanedInput = cleanInput(input);
+        if (cleanedInput.length === 0) {
+            rl.prompt();
+            return;
+        }
+
+        const command = cleanedInput[0];
+        console.log("Your command was:", command);
+        rl.prompt();
+    });
+
+    rl.on("close", () => {
+        console.log("Exiting REPL. Goodbye!");
+        process.exit(0);
+    });
 }
